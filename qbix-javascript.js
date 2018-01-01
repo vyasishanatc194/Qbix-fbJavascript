980ac8ff8e8b87d910564c5fc512719814b31049
Chetan Prajapati <chetan.citrusbug@gmail.com>
2018-01-01 18:35:11 +0530

    get freind of freind

---
+++ qbix-javascript.js
@@ -0,0 +1,68 @@
+//var accountHolder = document.getElementsByName("profile_id");
+//accountHolder = accountHolder[0].value;
+var liObj = document.querySelectorAll("._698");
+var fbFriendLogIn = new Array();		
+//fbFriendLogIn[accountHolder] = new Array();		
+for(var i = 0;i < liObj.length; i++){
+	var userName = liObj[i].querySelectorAll("._6a div.fsl a"),			
+	userFriend = liObj[i].querySelectorAll("._6a a._39g5"),			
+	userFriendPicture = liObj[i].querySelectorAll(".clearfix a._5q6s img"),			
+	friendUserId = liObj[i].querySelectorAll(".FriendRequestFriends");
+	frienduserIDdone = friendUserId[0].dataset.profileid;
+	userFriendPicture = (userFriendPicture.length > 0) ? userFriendPicture[0].currentSrc : "";
+	var mutualfbfriend = new Array();
+	var userObj = new Object();
+	
+	userObj = {				
+		name :  userName[0].innerHTML,
+		userId: friendUserId[0].dataset.profileid,
+		detail : (userFriend.length > 0) ? userFriend[0].innerHTML : "",
+		picture : userFriendPicture,
+		mutualFriend : new Array(),
+		url : 'https://www.facebook.com/profile.php?id='+frienduserIDdone+'&sk=friends'
+	};
+	//fbFriendLogIn[accountHolder].push(userObj);
+	fbFriendLogIn.push(userObj);
+}
+//console.log(fbFriendLogIn);
+var fbFriendLogInUser = new Array();
+var index = 0
+var intervalForMutualFriend = null;
+var getMutualFriend = function(){
+	//if(index < fbFriendLogIn.length){
+	if(index < 4){
+		var userFriend = new Object()
+		userFriend = fbFriendLogIn[index];
+		var mutualFriend    = window.open (userFriend.url, "_blank");
+		mutualFriend.addEventListener (
+			"load",
+			function () {
+				var mutualFriendDom = mutualFriend.document;
+				mutualFriendGetObj = mutualFriendDom.querySelectorAll('._698');
+				for(var j = 0;j < mutualFriendGetObj.length; j++){
+					var mutualFriendUserName = mutualFriendGetObj[j].querySelectorAll("._6a div.fsl a"),	
+					mutualuserFriend = mutualFriendGetObj[j].querySelectorAll("._6a a._39g5"),	
+					mutualuserFriendPicture = mutualFriendGetObj[j].querySelectorAll(".clearfix a._5q6s img"),	
+					friendUserId = mutualFriendGetObj[j].querySelectorAll(".FriendRequestFriends");	
+					mutualuserFriend = (mutualuserFriend.length > 0) ? mutualuserFriend[0].innerHTML : "";	
+					mutualuserFriendPicture = (mutualuserFriendPicture.length > 0) ? mutualuserFriendPicture[0].currentSrc : "";	
+					var userMutual = {
+						name :  mutualFriendUserName[0].innerHTML,
+						userId : friendUserId[0].dataset.profileid,
+						detail : mutualuserFriend,
+						picture : mutualuserFriendPicture
+					}
+					userFriend.mutualFriend.push(userMutual);
+				}
+				fbFriendLogInUser[index] = userFriend;
+			},
+			false
+		);
+		index = index+1;
+	}else{
+		clearInterval(intervalForMutualFriend);
+		console.log(fbFriendLogInUser);
+	}
+	
+}
+intervalForMutualFriend = setInterval(getMutualFriend, 5000);
